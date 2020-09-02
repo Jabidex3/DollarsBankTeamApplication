07225b74be391c4f47b45683cc9025fa8b7f63ef
Joshua anderson <Mp4ct@live.com>
2020-09-02 13:39:36 -0500

    Created base SpringBoot application and made some default MVC packages

---
+++ src/main/java/com/teamthree/dollarsbank/dollarsbankteamapplication/DollarsbankteamapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.teamthree.dollarsbank.dollarsbankteamapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsbankteamapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsbankteamapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamthree/dollarsbank/dollarsbankteamapplication/DollarsbankteamapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamthree.dollarsbank.dollarsbankteamapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsbankteamapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
