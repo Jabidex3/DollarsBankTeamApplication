f86a11f809e1b57a1276a016a32b15bb6b9596b5
Joshua anderson <Mp4ct@live.com>
2020-09-08 16:28:12 -0500

    Attempting to fix package names

--- src/main/java/com/teamthree/dollarsbank/dollarsbankteamapplication/repository/UserRepo.java
+++ src/main/java/com/teamthree/dollarsbank/dollarsbankteamapplication/repository/UserRepo.java
@@ -1,9 +1,9 @@
-package com.teamthree.dollarsbank.repo;
+package com.teamthree.dollarsbank.dollarsbankteamapplication.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.teamthree.dollarsbank.model.User;
+import com.teamthree.dollarsbank.dollarsbankteamapplication.model.User;
 
 // declares this interface as a repository
 // CrudRepository automatically connects to our MySQL database and contains save, findAll, delete and many more methods 
--- src/main/java/com/teamthree/dollarsbank/dollarsbankteamapplication/service/UserServiceImpl.java
+++ src/main/java/com/teamthree/dollarsbank/dollarsbankteamapplication/service/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.teamthree.dollarsbank.service;
+package com.teamthree.dollarsbank.dollarsbankteamapplication.service;
 
 import java.util.List;
 import java.util.Optional;
@@ -6,9 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-import com.teamthree.dollarsbank.model.User;
-import com.teamthree.dollarsbank.repo.UserRepo;
+import com.teamthree.dollarsbank.dollarsbankteamapplication.model.User;
+import com.teamthree.dollarsbank.dollarsbankteamapplication.repository.UserRepo;
 
 @Service
 public class UserServiceImpl implements UserService
